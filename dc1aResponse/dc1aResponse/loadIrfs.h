0bafed398dca2e686b90ebdbc579cfc2891fe77a
jchiang <>
2005-08-13 14:49:08 +0000

    initial import (from users/jchiang)

---
+++ dc1aResponse/dc1aResponse/loadIrfs.h
@@ -0,0 +1,8 @@
+#ifndef dc1aResponse_loadIrfs_h
+#define dc1aResponse_loadIrfs_h
+
+namespace dc1aResponse {
+   void loadIrfs();
+}
+
+#endif // dc1aResponse_loadIrfs_h
---
+++ dc1aResponse/src/Aeff.h
@@ -0,0 +1,61 @@
+/**
+ * @file Aeff.h
+ * @brief Aeff class declaration for dc1aResponse package
+ * @author J. Chiang <jchiang@slac.stanford.edu>
+ *
+ * $Header$
+ */
+  
+#ifndef dc1aResponse_Aeff_h
+#define dc1aResponse_Aeff_h
+
+#include <cmath>
+
+#include <string>
+#include <vector>
+
+#include "irfInterface/IAeff.h"
+
+namespace dc1aResponse {
+
+/**
+ * @class Aeff
+ * @brief Aeff class implementation for test
+ * @author J. Chiang <jchiang@slac.stanford.edu>
+ *
+ * $Header$
+ */
+  
+class Aeff : public irfInterface::IAeff {
+
+public:
+
+   Aeff(std::vector<double> & params) : m_params(params) {}
+
+   Aeff(double p0, double p1, double p2) {
+      m_params.clear();
+      m_params.push_back(p0);
+      m_params.push_back(p1);
+      m_params.push_back(p2);
+   }
+
+   virtual ~Aeff() {}
+
+   virtual double value(double energy, 
+                        const astro::SkyDir &srcDir, 
+                        const astro::SkyDir &scZAxis,
+                        const astro::SkyDir &scXAxis) const;
+
+   virtual double value(double energy, double theta, double) const;
+
+   virtual Aeff * clone() {return new Aeff(*this);}
+
+private:
+
+   std::vector<double> m_params;
+
+};
+
+} // namespace dc1aResponse
+
+#endif // dc1aResponse_Aeff_h
