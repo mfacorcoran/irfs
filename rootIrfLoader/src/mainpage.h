22b9c525572463079b5f93f7c614c5b8b221a0a0
jchiang <>
2005-08-13 15:24:08 +0000

    initial import (from original in users/jchiang)

---
+++ rootIrfLoader/src/mainpage.h
@@ -0,0 +1,19 @@
+/**
+   @mainpage rootIrfLoader
+
+   @author J. Chiang <jchiang@slac.stanford.edu>
+
+   @section intro Introduction
+
+   ROOT shared library that exposes wrapper classes for the abstract
+   interfaces in the irfInterface package.
+
+   <hr>
+   @section notes Release Notes
+   release.notes
+
+   <hr>
+   @section requirements Requirements
+   @verbinclude requirements
+ 
+*/
