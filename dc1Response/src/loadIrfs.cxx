f86ef4f31f966c9f2ab11bcfa8a1f1f15cc9e7e8
jchiang <>
2006-02-16 19:02:00 +0000

    fail if CALDB is not set

--- dc1Response/src/loadIrfs.cxx
+++ dc1Response/src/loadIrfs.cxx
@@ -37,74 +37,35 @@ void loadIrfs() {
    myFactory->getIrfsNames(irfsNames);
 
    long hdu;
-   if (getenv("CALDB")) {
-      try {
-         if ( !std::count(irfsNames.begin(), irfsNames.end(), "DC1::Front") ) {
-            irfUtil::Util::getCaldbFile("FRONT", "DETEFF", "DC1",
-                                        aeffFile, hdu);
-            aeff = new AeffDC1(aeffFile, static_cast<int>(hdu));
-            irfUtil::Util::getCaldbFile("FRONT", "RPSF", "DC1", psfFile, hdu);
-            psf = new PsfDC1(psfFile, static_cast<int>(hdu), 5);
-            irfUtil::Util::getCaldbFile("FRONT", "EREDIS", "DC1",
-                                        edispFile, hdu);
-            edisp = new EdispDC1(edispFile, static_cast<int>(hdu), 3);
-            myFactory->addIrfs("DC1::Front",
-                               new irfInterface::Irfs(aeff, psf, edisp, 0));
-         }
-         if ( !std::count(irfsNames.begin(), irfsNames.end(), "DC1::Back") ) {
-            irfUtil::Util::getCaldbFile("BACK", "DETEFF", "DC1",
-                                        aeffFile, hdu);
-            aeff = new AeffDC1(aeffFile, static_cast<int>(hdu));
-            irfUtil::Util::getCaldbFile("BACK", "RPSF", "DC1", psfFile, hdu);
-            psf = new PsfDC1(psfFile, static_cast<int>(hdu), 5);
-            irfUtil::Util::getCaldbFile("BACK", "EREDIS", "DC1",
-                                        edispFile, hdu);
-            edisp = new EdispDC1(edispFile, static_cast<int>(hdu), 3);
-            myFactory->addIrfs("DC1::Back",
-                               new irfInterface::Irfs(aeff, psf, edisp, 1));
-         }
-      } catch (std::invalid_argument &eObj) {
-         std::cout << "IrfsFactory::addDC1Irfs:\n"
-                   << eObj.what() << std::endl;
-      } catch (...) {
-         std::cout << "IrfsFactory::addDC1Irfs:\n"
-                   << "unknown exception" << std::endl;
-      }
-   } else {
-      char * caldbroot = ::getenv("CALDBROOT");
-      std::string caldbPath;
-      if (caldbroot == 0) {
-         throw std::runtime_error("Please set your CALDB env var.");
-      } else {
-         caldbPath = std::string(caldbroot) + "/CALDB/data/glast/lat/bcf/";
-      }            
 
-// Front
-      int npars(5);
-      if ( !std::count(irfsNames.begin(), irfsNames.end(), "DC1::Front") ) {
-         hdu = 2;
-         aeffFile = caldbPath + "ea/aeff_DC1.fits";
-         aeff = new AeffDC1(aeffFile, static_cast<int>(hdu));
-         psfFile = caldbPath + "psf/psf_DC1.fits";
-         psf = new PsfDC1(psfFile, hdu, npars);
-         edispFile = caldbPath + "edisp/edisp_DC1.fits";
-         npars = 3;
-         edisp = new EdispDC1(edispFile, hdu, npars);
-         myFactory->addIrfs("DC1::Front",
-                            new irfInterface::Irfs(aeff, psf, edisp, 0));
-      }
+   char * caldbroot = ::getenv("CALDB");
+   if (!caldbroot) {
+      throw std::runtime_error("CALDB is not set");
+   }
 
-// Back
-      if ( !std::count(irfsNames.begin(), irfsNames.end(), "DC1::Back") ) {
-         hdu = 3;
-         aeff = new AeffDC1(aeffFile, static_cast<int>(hdu));
-         npars = 5;
-         psf = new PsfDC1(psfFile, hdu, npars);
-         npars = 3;
-         edisp = new EdispDC1(edispFile, hdu, npars);
-         myFactory->addIrfs("DC1::Back",
-                            new irfInterface::Irfs(aeff, psf, edisp, 1));
-      }
+   if ( !std::count(irfsNames.begin(), irfsNames.end(), "DC1::Front") ) {
+      irfUtil::Util::getCaldbFile("FRONT", "DETEFF", "DC1",
+                                  aeffFile, hdu);
+      aeff = new AeffDC1(aeffFile, static_cast<int>(hdu));
+      irfUtil::Util::getCaldbFile("FRONT", "RPSF", "DC1", psfFile, hdu);
+      psf = new PsfDC1(psfFile, static_cast<int>(hdu), 5);
+      irfUtil::Util::getCaldbFile("FRONT", "EREDIS", "DC1",
+                                  edispFile, hdu);
+      edisp = new EdispDC1(edispFile, static_cast<int>(hdu), 3);
+      myFactory->addIrfs("DC1::Front",
+                         new irfInterface::Irfs(aeff, psf, edisp, 0));
+   }
+   if ( !std::count(irfsNames.begin(), irfsNames.end(), "DC1::Back") ) {
+      irfUtil::Util::getCaldbFile("BACK", "DETEFF", "DC1",
+                                  aeffFile, hdu);
+      aeff = new AeffDC1(aeffFile, static_cast<int>(hdu));
+      irfUtil::Util::getCaldbFile("BACK", "RPSF", "DC1", psfFile, hdu);
+      psf = new PsfDC1(psfFile, static_cast<int>(hdu), 5);
+      irfUtil::Util::getCaldbFile("BACK", "EREDIS", "DC1",
+                                  edispFile, hdu);
+      edisp = new EdispDC1(edispFile, static_cast<int>(hdu), 3);
+      myFactory->addIrfs("DC1::Back",
+                         new irfInterface::Irfs(aeff, psf, edisp, 1));
    }
 }
 
