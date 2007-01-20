53d8079baa20ca7f638c0591885740bb27674802
jchiang <>
2007-01-20 20:36:53 +0000

    add other irfInterface imps

--- src/Loader.cxx
+++ src/Loader.cxx
@@ -10,12 +10,20 @@
 
 #include "irfLoader/IrfLoaderFactory.h"
 
+#include "dc1Response/IrfLoader.h"
+#include "dc1aResponse/IrfLoader.h"
+#include "dc2Response/IrfLoader.h"
+#include "g25Response/IrfLoader.h"
 #include "handoff_response/MyIrfLoader.h"
 #include "testResponse/IrfLoader.h"
 
 namespace irfs {
 
 Loader::Loader() {
+   irfLoader::IrfLoaderFactory<dc1Response::IrfLoader>();
+   irfLoader::IrfLoaderFactory<dc1aResponse::IrfLoader>();
+   irfLoader::IrfLoaderFactory<dc2Response::IrfLoader>();
+   irfLoader::IrfLoaderFactory<g25Response::IrfLoader>();
    irfLoader::IrfLoaderFactory<handoff_response::MyIrfLoader>();
    irfLoader::IrfLoaderFactory<testResponse::IrfLoader>();
 }
