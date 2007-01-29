e21714565b190918c62bde9d9aa19b91ce47abfa
jchiang <>
2007-01-29 21:22:13 +0000

    comment out handoff_response

--- irfLoader/src/Registrar.cxx
+++ irfLoader/src/Registrar.cxx
@@ -12,7 +12,7 @@
 #include "dc1aResponse/IrfLoader.h"
 #include "dc2Response/IrfLoader.h"
 #include "g25Response/IrfLoader.h"
-#include "handoff_response/MyIrfLoader.h"
+//#include "handoff_response/MyIrfLoader.h"
 #include "testResponse/IrfLoader.h"
 
 #include "Registrar.h"
@@ -26,7 +26,7 @@ Registrar::Registrar() {
    registry.registerLoader(new dc1aResponse::IrfLoader());
    registry.registerLoader(new dc2Response::IrfLoader());
    registry.registerLoader(new g25Response::IrfLoader());
-   registry.registerLoader(new handoff_response::MyIrfLoader());
+//   registry.registerLoader(new handoff_response::MyIrfLoader());
    registry.registerLoader(new testResponse::IrfLoader());
 }
 
