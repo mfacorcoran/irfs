a136962c47c5119905503ca2caa4de9728ab8939
jchiang <>
2007-03-17 05:19:41 +0000

    use C++ version of dgaus8; remove statics; add adhocIntegrator

--- irfInterface/irfInterface/IEdisp.h
+++ irfInterface/irfInterface/IEdisp.h
@@ -144,50 +144,64 @@ public:
 
    virtual IEdisp * clone() = 0;
 
-   /// Return the integral of the energy dispersion function over
-   /// the specified interval in apparent energy.
-   /// @param self Pointer to IEdisp object.
-   /// @param emin Apparent energy lower bound (MeV)
-   /// @param emax Apparent energy upper bound (MeV)
-   /// @param energy True photon energy (MeV)
-   /// @param srcDir True photon direction.
-   /// @param scZAxis Spacecraft z-axis.
-   /// @param scXAxis Spacecraft x-axis.
-   /// @param time Photon arrival time (MET s).
-   static double edispIntegral(const IEdisp * self, double emin, double emax,
-                               double energy,
-                               const astro::SkyDir & srcDir, 
-                               const astro::SkyDir & scZAxis,
-                               const astro::SkyDir & scXAxis,
-                               double time=0);
-
-   /// Return the integral of the energy dispersion function 
-   /// using instrument coordinates.
-   /// @param self Pointer to IEdisp object.
-   /// @param emin Apparent energy lower bound (MeV)
-   /// @param emax Apparent energy upper bound (MeV)
-   /// @param energy True photon energy (MeV).
-   /// @param theta True inclination angle (degrees).
-   /// @param phi True azimuthal angle measured wrt the instrument
-   ///             X-axis (degrees).
-   /// @param time Photon arrival time (MET s).
-   static double edispIntegral(const IEdisp * self, double emin, double emax,
-                               double energy, double theta, double phi, 
-                               double time);
-
 private:
 
-   static double s_energy;
-   static double s_theta;
-   static double s_phi;
-   static double s_time;
-   static const IEdisp * s_self;
-   static double edispIntegrand(double * appEnergy);
-   static double meanEnergyIntegrand(double * appEnergy);
-   static double trueEnergyIntegrand(double * energy);
-   static double meanTrueEnergyIntegrand(double * energy);
-   static void setStaticVariables(double energy, double theta, double phi,
-                                  double time, const IEdisp * self);
+   class EdispIntegrand {
+   public:
+      EdispIntegrand(const IEdisp & edisp, double energy, double theta,
+                     double phi, double time) 
+         : m_edisp(edisp), m_energy(energy), m_theta(theta), 
+           m_phi(phi), m_time(time) {}
+      double operator()(double appEnergy) const {
+         return m_edisp.value(appEnergy, m_energy, m_theta, m_phi, m_time);
+      }
+   private:
+      const IEdisp & m_edisp;
+      double m_energy;
+      double m_theta;
+      double m_phi;
+      double m_time;
+   };
+
+   class MeanEnergyIntegrand : public EdispIntegrand {
+   public:
+      MeanEnergyIntegrand(const IEdisp & edisp, double energy, double theta,
+                          double phi, double time) 
+         : EdispIntegrand(edisp, energy, theta, phi, time) {}
+      double operator()(double appEnergy) const {
+         return appEnergy*EdispIntegrand::operator()(appEnergy);
+      }
+   };
+
+   class TrueEnergyIntegrand {
+   public:
+      TrueEnergyIntegrand(const IEdisp & edisp, double appEnergy, double theta,
+                          double phi, double time)
+         : m_edisp(edisp), m_appEnergy(appEnergy), m_theta(theta), m_phi(phi),
+           m_time(time) {}
+      double operator()(double energy) const {
+         return m_edisp.value(m_appEnergy, energy, m_theta, m_phi, m_time);
+      }
+   private:
+      const IEdisp & m_edisp;
+      double m_appEnergy;
+      double m_theta;
+      double m_phi;
+      double m_time;
+   };
+
+   class MeanTrueEnergyIntegrand : public TrueEnergyIntegrand {
+   public:
+      MeanTrueEnergyIntegrand(const IEdisp & edisp, double appEnergy, 
+                              double theta, double phi, double time)
+         : TrueEnergyIntegrand(edisp, appEnergy, theta, phi, time) {}
+      double operator()(double energy) const {
+         return energy*TrueEnergyIntegrand::operator()(energy);
+      }
+   };
+
+   double adhocIntegrator(const EdispIntegrand & func, double emin, 
+                          double emax) const;
 };
 
 } // namespace irfInterface
