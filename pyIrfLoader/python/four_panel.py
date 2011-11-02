c9e1e7cadb0dec8c874b5ea07e8354c9541c0f79
jchiang <>
2011-11-02 17:26:32 +0000

    update to use pylab and numpy and include all needed classes and functions

--- pyIrfLoader/python/four_panel.py
+++ pyIrfLoader/python/four_panel.py
@@ -1,28 +1,93 @@
+"""
+@brief Example "four panel" performance plots using pyIrfLoader.
+
+@author J. Chiang
+"""
+#
+# $Header$
+#
+
 import bisect
-from FunctionWrapper import FunctionWrapper
-from log_array import log_array
-import numarray as num
-import hippoplotter as plot
-import pyIrfLoader as irf_loader
+import numpy as num
+import pylab
+import pyIrfLoader
 
-irf_loader.Loader_go()
+class FunctionWrapper(object):
+    def __init__(self, func):
+        self.func = func
+    def __call__(self, xx, **kwds):
+        try:
+            y = []
+            for x in xx:
+                y.append(self.func(x, **kwds))
+            if isinstance(xx, num.ndarray):
+                y = num.array(y)
+            return y
+        except TypeError:
+            return self.func(xx, **kwds)
+    def __getattr__(self, attrname):
+        return getattr(self.func, attrname)
 
-factory = irf_loader.IrfsFactory_instance()
+_win_id = 0
 
-fronta = factory.create("DC2::FrontA")
-backa = factory.create("DC2::BackA")
-frontb = factory.create("DC2::FrontB")
-backb = factory.create("DC2::BackB")
+class Window(object):
+    def __init__(self, id=None):
+        global _win_id
+        if id is None:
+            id = _win_id
+            _win_id += 1
+        self.fig = pylab.figure(id)
+        self.axes = self.fig.add_subplot(111)
+        self.id = id
+    def set_title(self, title):
+        self.axes.set_title(title)
 
-dc1a_front = factory.create("DC1A::Front")
-dc1a_back = factory.create("DC1A::Back")
+def setAxis(xrange=None, yrange=None):
+    axisrange = list(pylab.axis())
+    if xrange is not None:
+        axisrange[:2] = xrange
+    if yrange is not None:
+        axisrange[2:] = yrange
+    pylab.axis(axisrange)
 
-psf_fa = fronta.psf()
-psf_ba = backa.psf()
-psf_fb = frontb.psf()
-psf_bb = backb.psf()
+def plot_curve(x, y, xlog=0, ylog=0, xname='x', yname='y', 
+               oplot=0, color='k', lineStyle='-', linewidth=1,
+               xrange=None, yrange=None):
+    if oplot == 0:
+        win = Window()
+    else:
+        win = None
+    marker = '%s%s' % (color, lineStyle)
+    if xlog and ylog:
+        pylab.loglog(x, y, marker, markersize=3, linewidth=linewidth)
+    elif xlog:
+        pylab.semilogx(x, y, marker, markersize=3, linewidth=linewidth)
+    elif ylog:
+        pylab.semilogy(x, y, marker, markersize=3, linewidth=linewidth)
+    else:
+        pylab.plot(x, y, marker, markersize=3, linewidth=linewidth)
+    if not oplot:
+        pylab.xlabel(xname)
+        pylab.ylabel(yname)
+    setAxis(xrange, yrange)
+    return win
 
-radii = log_array(30, 1e-2, 30.)
+logspace = lambda xmin, xmax, nx : num.logspace(num.log10(xmin),
+                                                num.log10(xmax), nx)
+
+pyIrfLoader.Loader_go()
+
+factory = pyIrfLoader.IrfsFactory_instance()
+
+irfName = "P7SOURCE_V6MC"
+
+front = factory.create(irfName + "::FRONT")
+back = factory.create(irfName + "::BACK")
+
+psf_f = front.psf()
+psf_b = back.psf()
+
+radii = logspace(1e-2, 30., 30)
 
 @FunctionWrapper
 def theta_68(energy, psf=None, inc=0, phi=0, frac=0.68):
@@ -32,70 +97,36 @@ def theta_68(energy, psf=None, inc=0, phi=0, frac=0.68):
     return ((frac - y[indx])/(y[indx+1] - y[indx])
             *(radii[indx+1] - radii[indx]) + radii[indx])
 
-energies = log_array(40, 20, 2e5)
-plot1 = plot.scatter(energies, theta_68(energies, psf=psf_fa), pointRep='Line',
-                     xlog=1, ylog=1, xname='Energy (MeV)',
-                     yname='theta_68 (deg)')
-plot1.setTitle('normal incidence')
-plot.scatter(energies, theta_68(energies, psf=psf_ba), pointRep='Line',
-             oplot=1, lineStyle='Dot')
-plot.scatter(energies, theta_68(energies, psf=psf_fb), pointRep='Line',
-             xlog=1, ylog=1, color='red', oplot=1)
-plot.scatter(energies, theta_68(energies, psf=psf_bb), pointRep='Line',
-             oplot=1, color='red', lineStyle='Dot', autoscale=1)
+energies = logspace(20., 3e5, 40)
+plot1 = plot_curve(energies, theta_68(energies, psf=psf_f),
+                   xlog=1, ylog=1, xname='Energy (MeV)',
+                   yname='theta_68 (deg)')
+plot1.set_title('normal incidence')
+plot_curve(energies, theta_68(energies, psf=psf_b), oplot=1, lineStyle=':')
 
-plot.scatter(energies, theta_68(energies, psf=dc1a_front.psf()),
-             pointRep='Line', oplot=1, color='blue')
-plot.scatter(energies, theta_68(energies, psf=dc1a_back.psf()),
-             pointRep='Line', oplot=1, color='blue', lineStyle='Dot')
-
-aeff_fa = fronta.aeff()
-aeff_ba = backa.aeff()
-aeff_fb = frontb.aeff()
-aeff_bb = backb.aeff()
+aeff_f = front.aeff()
+aeff_b = back.aeff()
 
 @FunctionWrapper
 def aeff(energy, aeffObj=None, inc=0, phi=0):
     return aeffObj.value(energy, inc, phi)
 
-plot2 = plot.scatter(energies, aeff(energies, aeffObj=aeff_fa), xlog=1,
-                     pointRep='Line', xname='Energy (MeV)',
-                     yname='eff. area (cm^2)')
-plot2.setTitle('normal incidence')
-plot.scatter(energies, aeff(energies, aeffObj=aeff_ba), oplot=1,
-             pointRep='Line', lineStyle='Dot')
-plot.scatter(energies, aeff(energies, aeffObj=aeff_fb), oplot=1,
-             pointRep='Line', color='red')
-plot.scatter(energies, aeff(energies, aeffObj=aeff_bb), oplot=1,
-             pointRep='Line', color='red', lineStyle='Dot')
-
-plot.scatter(energies, aeff(energies, aeffObj=dc1a_front.aeff()), oplot=1,
-             pointRep='Line', color='blue', autoscale=1)
-plot.scatter(energies, aeff(energies, aeffObj=dc1a_back.aeff()), oplot=1,
-             pointRep='Line', color='blue', autoscale=1, lineStyle='Dot')
+plot2 = plot_curve(energies, aeff(energies, aeffObj=aeff_f), xlog=1,
+                   xname='Energy (MeV)', yname='eff. area (cm^2)')
+plot2.set_title('normal incidence')
+plot_curve(energies, aeff(energies, aeffObj=aeff_b), oplot=1, lineStyle=':')
 
 @FunctionWrapper
 def aeff_profile(inc, aeffObj=None, energy=1e3, phi=0):
     return aeffObj.value(energy, inc, phi)
 
-thetas = num.arange(70)
+thetas = num.arange(70, dtype=num.float)
 
-plot3 = plot.scatter(thetas, aeff_profile(thetas, aeffObj=aeff_fa),
-                     pointRep='Line', xname='inclination (deg)',
-                     yname='eff. area (cm^2)')
-plot3.setTitle('E = 1 GeV')
-plot.scatter(thetas, aeff_profile(thetas, aeffObj=aeff_ba), oplot=1,
-             pointRep='Line', lineStyle='Dot', autoscale=1)
-plot.scatter(thetas, aeff_profile(thetas, aeffObj=aeff_fb), oplot=1,
-             pointRep='Line', color='red', autoscale=1)
-plot.scatter(thetas, aeff_profile(thetas, aeffObj=aeff_bb), oplot=1,
-             pointRep='Line', color='red', lineStyle='Dot', autoscale=1)
-
-plot.scatter(thetas, aeff_profile(thetas, aeffObj=dc1a_front.aeff()),
-             oplot=1, pointRep='Line', color='blue', autoscale=1)
-plot.scatter(thetas, aeff_profile(thetas, aeffObj=dc1a_back.aeff()),
-             oplot=1, pointRep='Line', color='blue', autoscale=1,
-             lineStyle='Dot')
+plot3 = plot_curve(thetas, aeff_profile(thetas, aeffObj=aeff_f),
+                   xname='inclination (deg)', yname='eff. area (cm^2)')
+plot3.set_title('E = 1 GeV')
+plot_curve(thetas, aeff_profile(thetas, aeffObj=aeff_b), oplot=1,
+           lineStyle=':')
 
 @FunctionWrapper
 def th68_profile(inc, psf=None, energy=1e3, phi=0, frac=0.68):
@@ -105,17 +136,8 @@ def th68_profile(inc, psf=None, energy=1e3, phi=0, frac=0.68):
     return ((frac - y[indx])/(y[indx+1] - y[indx])
             *(radii[indx+1] - radii[indx]) + radii[indx])
 
-plot4 = plot.scatter(thetas, th68_profile(thetas, psf=psf_fa),
-                     pointRep='Line', xname='inclination (deg)',
-                     yname='theta_68 (deg)')
-plot4.setTitle('E = 1 GeV')
-plot.scatter(thetas, th68_profile(thetas, psf=psf_ba), oplot=1,
-             pointRep='Line', lineStyle='Dot', autoscale=1)
-plot.scatter(thetas, th68_profile(thetas, psf=psf_fb), oplot=1,
-             pointRep='Line', color='red', autoscale=1)
-plot.scatter(thetas, th68_profile(thetas, psf=psf_bb), oplot=1,
-             pointRep='Line', color='red', lineStyle='Dot', autoscale=1)
-plot.scatter(thetas, th68_profile(thetas, psf=dc1a_front.psf()), oplot=1,
-             pointRep='Line', color='blue', autoscale=1)
-plot.scatter(thetas, th68_profile(thetas, psf=dc1a_back.psf()), oplot=1,
-             pointRep='Line', color='blue', lineStyle='Dot', autoscale=1)
+plot4 = plot_curve(thetas, th68_profile(thetas, psf=psf_f),
+                   xname='inclination (deg)', yname='theta_68 (deg)')
+plot4.set_title('E = 1 GeV')
+plot_curve(thetas, th68_profile(thetas, psf=psf_b), oplot=1,
+           lineStyle=':')
