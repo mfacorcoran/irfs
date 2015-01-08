fb50c356a06c332d9c2a02d2a175c174d9cbf867
jchiang <>
2015-01-08 23:35:17 +0000

    remove dc1,dc2,g25 stuff

--- irfLoader/src/Registrar.cxx
+++ irfLoader/src/Registrar.cxx
@@ -18,13 +18,9 @@
 
 #include "irfInterface/IrfRegistry.h"
 
-// #include "dc1Response/IrfLoader.h"
 #include "dc1aResponse/IrfLoader.h"
-// #include "dc2Response/IrfLoader.h"
-// #include "g25Response/IrfLoader.h"
 #include "latResponse/IrfLoader.h"
 #include "testResponse/IrfLoader.h"
-//#include "egretResponse/IrfLoader.h"
 
 #include "Registrar.h"
 
@@ -46,13 +42,9 @@ Registrar::Registrar() {
    
    irfInterface::IrfRegistry & registry(irfInterface::IrfRegistry::instance());
 
-//    registry.registerLoader(new dc1Response::IrfLoader());
    registry.registerLoader(new dc1aResponse::IrfLoader());
-//    registry.registerLoader(new dc2Response::IrfLoader());
-//    registry.registerLoader(new g25Response::IrfLoader());
    registry.registerLoader(new latResponse::IrfLoader());
    registry.registerLoader(new testResponse::IrfLoader());
-//    registry.registerLoader(new egretResponse::IrfLoader());
 
    std::vector<std::string> names(registry.irfNames());
 }
