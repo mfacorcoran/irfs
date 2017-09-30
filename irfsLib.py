43715d41176a7cf677d93f5b183f3346b2f70cbb
Joanne Bogart <jrb@slac.stanford.edu>
2017-09-29 17:55:03 -0700

    changes for ST_User

--- irfsLib.py
+++ irfsLib.py
@@ -2,9 +2,10 @@ def generate(env, **kw):
     env.Tool('irfInterfaceLib')
     env.Tool('irfUtilLib')
     env.Tool('irfLoaderLib')
-    env.Tool('dc1aResponseLib')
     env.Tool('testResponseLib')
-    env.Tool('handoff_responseLib')
+    if baseEnv.get('CONTAINERNAME','') != 'ScienceTools_User':
+        env.Tool('dc1aResponseLib')
+        env.Tool('handoff_responseLib')
 
 def exists(env):
     return 1
