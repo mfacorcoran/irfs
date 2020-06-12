a55233c9fb64cbdb1a4f9ee31bfb99a1c3f088d5
jasercion <joseph.a.asercion@nasa.gov>
2020-06-12 15:43:24 -0400

    Fix P8R3_V2 entries

--- caldb/python/update_caldb_indx.py
+++ caldb/python/update_caldb_indx.py
@@ -18,19 +18,19 @@ import os
 #            "P7SOURCE_V6MC", 
 #            "P7REP_TRANSIENT_V10", "P7REP_SOURCE_V10", 
 #            "P7REP_CLEAN_V10", "P7REP_ULTRACLEAN_V10")
-suffixes = ("P8R3_TRANSIENT100E_V3",
-            "P8R3_TRANSIENT100_V3",
-            "P8R3_TRANSIENT020E_V3",
-            "P8R3_TRANSIENT020_V3",
-            "P8R3_TRANSIENT010E_V3",
-            "P8R3_TRANSIENT010_V3",
-            "P8R3_SOURCE_V3",
-            "P8R3_CLEAN_V3",
-            "P8R3_ULTRACLEAN_V3",
-            "P8R3_ULTRACLEANVETO_V3",
-            "P8R3_SOURCEVETO_V3",
-            "P8R3_TRANSIENT100S_V3",
-            "P8R3_TRANSIENT015S_V3")
+suffixes = ("P8R3_TRANSIENT100E_V2",
+            "P8R3_TRANSIENT100_V2",
+            "P8R3_TRANSIENT020E_V2",
+            "P8R3_TRANSIENT020_V2",
+            "P8R3_TRANSIENT010E_V2",
+            "P8R3_TRANSIENT010_V2",
+            "P8R3_SOURCE_V2",
+            "P8R3_CLEAN_V2",
+            "P8R3_ULTRACLEAN_V2",
+            "P8R3_ULTRACLEANVETO_V2",
+            "P8R3_SOURCEVETO_V2",
+            "P8R3_TRANSIENT100S_V2",
+            "P8R3_TRANSIENT015S_V2")
 
 for item in suffixes:
     for prefix, loc in zip(("aeff", "psf", "edisp"), ("ea", "psf", "edisp")):
